d3b0e95eeca44b447ac7c27b162bb8a1cc6d35ee
chidinmajude <danielleiheaturu@gmail.com>
2022-06-30 19:41:42 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-echo '#include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main-print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19/n", 59);
 return (1);
-}'>101-quote.c
+}
