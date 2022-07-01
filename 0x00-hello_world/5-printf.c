51bee37628b3181bb7ab2280229aa4bed7606041
chidinmajude <danielleiheaturu@gmail.com>
2022-06-30 18:15:32 -0700

    Hello print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 /*
 * main - Entry point
 *
- * Return: Always 0 (success)
- */
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
